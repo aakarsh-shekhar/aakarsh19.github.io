2e6e6e06c367db23e2a88d39824a24c1f6bc860f
aakarsh19 <aakarsh.shekhar@mail.mcgill.ca>
2018-04-28 23:22:19 -0400

    first version complete

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@
 
 var ideabinText = "Built using Node.Js for back-end, and CSS/HTML for front-end <br>IdeaBin helps connect young programmers with users who listed their problems, to help ﬁx the problem for prizes.<br> It was built during a 36-hour long hackathon at Yale University.";
 var psychbotText = "PsychBot is a chatbot that helps deal with general psychological problems. Built in Node.js/Javascript, using Cisco Spark ChatBot API for chat-box, and Nuance Nina Knowledge (NiK) API to analyze text or speech. Additionally, it was one of the first third-party Cisco Spark chatbot to implement voice functionality, which was built using the Bing Speech API.";
-
+var loandataText = "The Loan data exploration plotted the different conditions against each other to find out what really affects interest rates. Additionally, the prediction was based on the best fitting model out of the many we implemented. Built using python and a standard Jupyter and SkLearn workflow, along with a Google compute background, during a 36-hour long hackathon.";
 
 document.getElementById("c1").addEventListener('click', function() {
 	var tmp = document.getElementById("ideabin").innerText;
@@ -14,6 +14,12 @@ document.getElementById("c2").addEventListener('click', function(){
 	var tmp = document.getElementById("psychbot").innerText;
 	document.getElementById("psychbot").innerHTML = psychbotText;
 	psychbotText = tmp;
+});
+
+document.getElementById("c3").addEventListener('click', function(){
+	var tmp = document.getElementById("loandata").innerText;
+	document.getElementById("loandata").innerHTML = loandataText;
+	loandataText = tmp;
 })
 
 /*
