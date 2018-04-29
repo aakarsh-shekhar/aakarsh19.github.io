5985a895ee42071a53a164585b98df11a61ba696
aakarsh19 <aakarsh.shekhar@mail.mcgill.ca>
2018-04-28 22:27:12 -0400

    added about me page

--- js/projects.js
+++ js/projects.js
@@ -3,6 +3,20 @@
 var ideabinText = "Built using Node.Js for back-end, and CSS/HTML for front-end <br>IdeaBin helps connect young programmers with users who listed their problems, to help ﬁx the problem for prizes.<br> It was built during a 36-hour long hackathon at Yale University.";
 var psychbotText = "PsychBot is a chatbot that helps deal with general psychological problems. Built in Node.js/Javascript, using Cisco Spark ChatBot API for chat-box, and Nuance Nina Knowledge (NiK) API to analyze text or speech. Additionally, it was one of the first third-party Cisco Spark chatbot to implement voice functionality, which was built using the Bing Speech API.";
 
+
+document.getElementById("c1").addEventListener('click', function() {
+	var tmp = document.getElementById("ideabin").innerText;
+	document.getElementById("ideabin").innerHTML = ideabinText;
+	ideabinText = tmp;
+});
+
+document.getElementById("c2").addEventListener('click', function(){
+	var tmp = document.getElementById("psychbot").innerText;
+	document.getElementById("psychbot").innerHTML = psychbotText;
+	psychbotText = tmp;
+})
+
+/*
 var proj = document.getElementsByClassName("card");
 
 for (var i = 1; i <= proj.length; i++) {
@@ -13,6 +27,7 @@ for (var i = 1; i <= proj.length; i++) {
 		displayProject(this.id);
 	});
 };
+
 
 function displayProject(x) {
 	if (x === "c1") {
@@ -26,3 +41,4 @@ function displayProject(x) {
 		psychbotText = tmp;
 	}
 };
+*/
